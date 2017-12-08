2859a1d92b6795e4052341a1f3a46bc5940a4b9b
Brett Jensen <brett.jensen@infinityworks.com>
2017-12-08 08:05:05 +0000

    Add author page and mock authors api

---
+++ src/api/authorData.js
@@ -0,0 +1,20 @@
+module.exports = {
+  authors:
+  [
+    {
+      id: 'cory-house',
+      firstName: 'Cory',
+      lastName: 'House'
+    },
+    {
+      id: 'scatt-allen',
+      firstName: 'Scott',
+      lastName: 'Allen'
+    },
+    {
+      id: 'dan-wahlin',
+      firstName: 'Dan',
+      lastName: 'Wahlin'
+    }
+  ]
+};
